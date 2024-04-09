bd101a7661ffaa2413f736ea863ec0a55bda87b3
Channa Jayamuni <hjchanna@gmail.com>
2024-04-09 13:15:24 +0200

    mtls test

---
+++ src/test/java/me/channa/test/mtlsdemo/MtlsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.channa.test.mtlsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtlsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
