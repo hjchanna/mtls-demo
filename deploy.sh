ae2f65bf7a03eba8fba7f578ed2a0a1cd1759d39
Channa Jayamuni <hjchanna@gmail.com>
2024-04-09 15:38:04 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 apictl mg login dev -u admin -p admin -k
+# NOTE: Create a new environment called 'dev' in the API Controller before running this command
 
 apictl mg deploy api -e dev -f wso2-cc-mtls-test/mtls-demo-api -k
